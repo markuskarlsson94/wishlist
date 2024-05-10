b553af2001721f96afd861f2ccd8e9cca62d00c7
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-05-10 15:34:16 +0200

    chore(backend): added dummy users for testing

---
+++ backend/users.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        username: "admin",
+        password: "abc123"
+    },
+    {
+        username: "customer",
+        password: "foobar"
+    }
+];
+
+export default users;
