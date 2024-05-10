58caf478d83959ec13bc44be57911809e1bb36f9
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-05-10 15:32:40 +0200

    feat(backend): added dotenv and port env variable

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express, { json } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 app.use(json());
 
+const port = process.env.PORT || 3000;
+
 app.listen(3000, () => {
-    console.log("Auth server started.");
+    console.log(`Server started on port ${port}.`);
 });
