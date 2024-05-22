023375a62a76c084558bfe2b47c87e582e8a322e
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-05-22 22:37:33 +0200

    feat(backend): added db test environment

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -11,6 +11,16 @@ const config = {
             database: process.env.DB_DATABASE,
         	port: process.env.DB_PORT,
         }
+    },
+    test: {
+        client: "pg",
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        	port: process.env.DB_PORT,
+        }
     }
 };
 
