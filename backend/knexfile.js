19734fe7d9420cccc4df632e72603565d3967334
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-05-26 21:03:21 +0200

    chore(backend): WIP: test db setup

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -18,7 +18,8 @@ const config = {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
+            //database: process.env.DB_DATABASE_TEST,
+            database: process.env.DB_DATABASE, // TODO: Connect to test database.
         	port: process.env.DB_PORT,
         }
     }
---
+++ init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE dbTest;
+GRANT ALL PRIVILEGES ON DATABASE dbTest TO myuser;
+\l -- List database
