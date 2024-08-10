38c471786f3cf9ae71d58a106183caf625a1789f
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-08-10 13:24:05 +0200

    feat(frontend): added useUser hook

---
+++ frontend/src/hooks/useUser.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../axiosInstance";
+
+type UserResponse = {
+    data: {
+        id: string
+    };
+}
+
+const useUser = () => {
+    const { data, ...rest } = useQuery<UserResponse>({
+        queryKey: ["user"],
+        queryFn: () => axiosInstance.get("/auth/me"),
+    });
+
+    return { user: data?.data.id, ...rest };
+};
+
+export default useUser;
