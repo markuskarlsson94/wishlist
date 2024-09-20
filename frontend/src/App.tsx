33ad7601a79d1eb87a0ecc4da111fa588ce47d47
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-09-20 23:43:04 +0200

    feat(frontend): use AuthProvider and ProtectedRoutes in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,22 @@ import './App.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import ProtectedRoutes from './components/ProtectedRoutes';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />}></Route>
-                <Route path="/home" element={<Home />}></Route>
-            </Routes>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route element={<ProtectedRoutes />}>
+                        <Route path="/home" element={<Home />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
-}
+};
 
 export default App;
