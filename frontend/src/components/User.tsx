c4b52c1caf8cab3dde3f38a87a226d4fca36bdc8
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-10-04 22:11:24 +0200

    feat(frontend): fetch user info in user component

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -1,11 +1,28 @@
 import { NavLink, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../axiosInstance";
+import { useEffect, useState } from "react";
+import UserType from "../types/UserType";
 
 const User = () => {
+    const [user, setUser] = useState<UserType>();
     const { userId } = useParams();
+
+    const { data, isSuccess } = useQuery({
+        queryKey: ["user", userId],
+        queryFn: () => axiosInstance.get(`/user/${userId}`),
+    });
+
+    useEffect(() => {
+        if (isSuccess) {
+            setUser(data.data.user);
+        }
+    }, [data, isSuccess]);
 
     return (
         <>
             <h2>User</h2>
+            <p>{user?.firstName} {user?.lastName}</p>
             <div>
                 <NavLink to={`/user/${userId}/wishlists`}>Wishlists</NavLink>
             </div>
