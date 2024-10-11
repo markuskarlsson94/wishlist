7a33ea9ff9895a9e0b010aff31bd101e7baf8727
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-10-11 16:50:46 +0200

    feat(frontend): added Topbar component

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
+import Topbar from "./Topbar";
 
 const MainLayout = () => {
     return (
         <>
+            <Topbar />
             <Sidebar />
             <Outlet />
         </>
---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,9 @@
+const Topbar = () => {
+    return (
+        <>
+            <h2>Topbar</h2>
+        </>
+    );
+};
+
+export default Topbar;
