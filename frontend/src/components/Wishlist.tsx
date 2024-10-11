2a3f1c4cebcd2e97345cbfa3cdc97e734e3b945a
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-10-11 16:56:41 +0200

    feat(frontend): use wishlist hooks in Wishlist component

--- frontend/src/components/Wishlist.tsx
+++ frontend/src/components/Wishlist.tsx
@@ -1,47 +1,30 @@
 import { useState, useEffect } from "react";
-import WishlistType from "../types/WishlistType";
 import ItemInputType from "../types/ItemInputType";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import axiosInstance from "../axiosInstance";
 import { useParams, useNavigate, NavLink } from "react-router-dom";
 import CreateItemForm from "../forms/CreateItemForm";
 import { useAuth } from "../contexts/AuthContext";
 import { useCreateItem, useGetItems } from "../hooks/item";
+import { useDeleteWishlist, useGetWishlist } from "../hooks/wishlist";
 
 const Wishlist = () => {
-    const [wishlist, setWishlist] = useState<WishlistType>();
     const [isOwner, setIsOwner] = useState<boolean>(false);
     const params = useParams<{ id: string }>();
     const id = Number(params.id);
-    const navigate = useNavigate();
-    const queryClient = useQueryClient();
-    const [showCreate, setShowCreate] = useState<boolean>(false);
+    const { wishlist, isSuccess } = useGetWishlist(id);
     const { userId } = useAuth();
+    const navigate = useNavigate();
+    const deleteWishlist = useDeleteWishlist({ onSuccess: () => {
+        navigate(`/user/${userId}/wishlists`, { replace: true });
+    } });
+    const [showCreate, setShowCreate] = useState<boolean>(false);
     const { items } = useGetItems(id);
     const { createItem } = useCreateItem();
 
-    const { data, isSuccess } = useQuery({
-        queryKey: ["wishlist", id],
-        queryFn: () => axiosInstance.get(`wishlist/${id}`),
-    });
-
     useEffect(() => {
         if (isSuccess) {
-            const wishlist = data.data.wishlist;
-            setWishlist(wishlist);
-            setIsOwner(wishlist.owner === userId);
+            setIsOwner(wishlist?.owner === userId);
         }
-    }, [data, isSuccess]);
-
-    const deleteWishlist = async () => {
-        await axiosInstance.delete(`/wishlist/${id}`);
-        queryClient.invalidateQueries({ queryKey: ["wishlists"] });
-        navigate("/wishlists");
-    };
-
-    const deleteWishlistMutation = useMutation({
-        mutationFn: deleteWishlist,
-    });
+    }, [wishlist, isSuccess]);
 
     const Item = (item: any) => {
         return (
@@ -70,7 +53,7 @@ const Wishlist = () => {
     }
 
     const handleDelete = () => {
-        deleteWishlistMutation.mutate();
+        deleteWishlist(id);
     };
 
     const handleBack = () => {
