aca263caea6a542503253eb304e63b288de09364
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-10-14 17:44:18 +0200

    feat(frontend): added Reservation component

---
+++ frontend/src/components/Reservations.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import { useDeleteReservation, useGetReservations } from "../hooks/reservation";
+import { useAuth } from "../contexts/AuthContext";
+import ReservationType from "../types/ReservationType";
+import { useGetItem } from "../hooks/item";
+import { NavLink } from "react-router-dom";
+
+const ReservationItem = ({ reservation }: { reservation: ReservationType }) => {
+    const { userId } = useAuth();
+    const { item, isSuccess } = useGetItem(reservation.item);
+    const deleteReservation = useDeleteReservation({ userId });
+
+    if (!isSuccess || !item) return null;
+
+    const handleDelete = () => {
+        deleteReservation(reservation.id, item.id);
+    };
+
+    return (
+        <div>
+            <NavLink to={`/item/${item.id}`}>{item?.title}</NavLink>    
+            <button onClick={handleDelete}>Unreserve</button>
+        </div>
+    );
+};
+
+const Reservations = () => {
+    const { userId } = useAuth();
+    const { reservations } = useGetReservations(userId);
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate(-1);
+    };
+
+    return (
+        <>
+            <h2>Reservations</h2>
+            <button onClick={handleBack}>Back</button>
+            {reservations?.map(reservation => (
+                <ReservationItem key={reservation.id} reservation={reservation} />
+            ))}
+        </>
+    );
+};
+
+export default Reservations;
