c50d437855f6e4d4c5630080106477b9777ac219
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-10-14 17:49:41 +0200

    feat(frontend): use reservation hooks in Item component

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -2,20 +2,23 @@ import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { useGetItem, useDeleteItem } from "../hooks/item";
+import { useCreateReservation, useDeleteReservation } from "../hooks/reservation";
 
 const Item = () => {
     const [isOwner, setIsOwner] = useState<boolean>(false);
-    const params = useParams<{ id: string}>();
+    const params = useParams<{ id: string }>();
     const id = Number(params.id);
     const navigate = useNavigate();
     const { userId } = useAuth();
     const { item, isSuccess } = useGetItem(id);
+    const createReservation = useCreateReservation({ userId });
+    const deleteReservation = useDeleteReservation({ userId });
 
     const onDeleteItem = () => {
         if (item?.wishlist) {
-            navigate(`/wishlist/${item.wishlist}`);
+            navigate(`/wishlist/${item.wishlist}`, { replace: true });
         }
-    }
+    };
 
     const { deleteItem } = useDeleteItem({ onSuccess: onDeleteItem });
 
@@ -29,9 +32,29 @@ const Item = () => {
         deleteItem(Number(id));
     };
 
+    const handleReserve = () => {
+        if (item) {
+            createReservation(item.id);
+        }
+    };
+
+    const handleUnreserve = () => {
+        if (item?.reservation) {
+            deleteReservation(item.reservation, item.id);
+        }
+    };
+
     const handleBack = () => {
         navigate(-1);
-    }
+    };
+
+    const reserveButton = () => {
+        return <button onClick={handleReserve}>Reserve</button>
+    };
+
+    const unreserveButton = () => {
+        return <button onClick={handleUnreserve}>Unreserve</button>
+    };
 
     return (
         <>
@@ -40,8 +63,9 @@ const Item = () => {
             <h3>{item?.title}</h3>
             <p>{item?.description}</p>
             {isOwner && <button onClick={handleDelete}>Delete item</button>}
+            {!isOwner && (item?.reservation ? unreserveButton() : reserveButton())}
         </>
-    )
+    );
 };
 
 export default Item;
