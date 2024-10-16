d1e0fa9e308d8039e7fa348217b87c35dfcfd2d0
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-10-16 17:44:16 +0200

    feat(frontend): added item update in Item component

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import { useGetItem, useDeleteItem } from "../hooks/item";
+import { useGetItem, useDeleteItem, useUpdateItem } from "../hooks/item";
 import { useCreateReservation, useDeleteReservation } from "../hooks/reservation";
+import ItemInputType from "../types/ItemInputType";
+import ItemForm from "../forms/ItemForm";
 
 const Item = () => {
     const [isOwner, setIsOwner] = useState<boolean>(false);
+    const [showUpdateItem, setShowUpdateItem] = useState<boolean>(false);
     const params = useParams<{ id: string }>();
     const id = Number(params.id);
     const navigate = useNavigate();
@@ -13,6 +16,7 @@ const Item = () => {
     const { item, isSuccess } = useGetItem(id);
     const createReservation = useCreateReservation({ userId });
     const deleteReservation = useDeleteReservation({ userId });
+    const updateItem = useUpdateItem();
 
     const onDeleteItem = () => {
         if (item?.wishlist) {
@@ -56,12 +60,37 @@ const Item = () => {
         return <button onClick={handleUnreserve}>Unreserve</button>
     };
 
+    const handleShowUpdateItem = () => {
+        setShowUpdateItem(true);
+    };
+
+    const handleUpdateItem = (data: ItemInputType) => {
+        if (item) {
+            updateItem(item.id, data);
+        }
+
+        setShowUpdateItem(false);
+    };
+
+    const handleCancel = () => {
+        setShowUpdateItem(false);
+    };
+
+    const itemValues: ItemInputType = {
+        title: item?.title || "",
+        description: item?.description || "",
+    };
+
     return (
         <>
             <h2>Item</h2>
             <button onClick={handleBack}>Back</button>
             <h3>{item?.title}</h3>
             <p>{item?.description}</p>
+            {isOwner && (showUpdateItem ? 
+                ItemForm(itemValues, handleUpdateItem, handleCancel) : 
+                <button onClick={handleShowUpdateItem}>Update item</button>)
+            }
             {isOwner && <button onClick={handleDelete}>Delete item</button>}
             {!isOwner && (item?.reservation ? unreserveButton() : reserveButton())}
         </>
