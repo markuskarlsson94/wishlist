69e559bad9d7bcb5f6e805a00a9a41bde76ff064
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-10-20 23:17:43 +0200

    feat(frontend): added friend requests to Friend component

--- frontend/src/components/Friends.tsx
+++ frontend/src/components/Friends.tsx
@@ -1,44 +1,96 @@
-import { useState, useEffect } from "react";
 import { useNavigate, NavLink, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import axiosInstance from "../axiosInstance";
-import UserType from "../types/UserType";
+import { useAcceptFriendRequest, useDeleteFriendRequest, useGetFriendRequests } from "../hooks/friendRequest";
+import FriendRequest from "../types/FriendRequesstType";
+import { useAuth } from "../contexts/AuthContext";
+import { useGetUser } from "../hooks/user";
+import { useGetFriends } from "../hooks/friend";
+
+const ReceivedFriendRequest = ({ friendRequest }: { friendRequest: FriendRequest }) => {
+    const { userId } = useAuth();
+    const { user } = useGetUser(friendRequest.sender);
+    const deleteFriendRequest = useDeleteFriendRequest({ userId });
+    const acceptFriendRequest = useAcceptFriendRequest({ userId });
+
+    const handleAccept = () => {
+        acceptFriendRequest(friendRequest.id);
+    };
+
+    const handleDecline = () => {
+        deleteFriendRequest(friendRequest.id);
+    };
+
+    return (
+        <div key={friendRequest.id}>
+            <NavLink to={`/user/${friendRequest.sender}`}>
+                {user?.firstName} {user?.lastName}
+            </NavLink>
+            <button onClick={handleAccept}>Accept</button>
+            <button onClick={handleDecline}>Decline</button>
+        </div>
+    );
+};
+
+const SentFriendRequest = ({ friendRequest }: { friendRequest: FriendRequest }) => {
+    const { userId } = useAuth();
+    const { user } = useGetUser(friendRequest.receiver);
+    const deleteFriendRequest = useDeleteFriendRequest({ userId });
+
+    const handleCancel = () => {
+        deleteFriendRequest(friendRequest.id);
+    };
+
+    return (
+        <div key={friendRequest.id}>
+            <NavLink to={`/user/${friendRequest.receiver}`}>
+                {user?.firstName} {user?.lastName}
+            </NavLink>
+            <button onClick={handleCancel}>Cancel</button>
+        </div>
+    );
+};
+
+const Friend = ({ friend }: { friend: number }) => {
+    const { user } = useGetUser(friend);
+
+    return (
+        <div>
+            <NavLink to={`/user/${friend}`}>
+                {user?.firstName} {user?.lastName}
+            </NavLink>
+        </div>
+    );
+};
 
 const Friends = () => {
-    const [friends, setFriends] = useState<UserType[]>([]);
-    const { userId } = useParams();
+    const params = useParams<{ userId: string }>();
+    const userId = Number(params.userId);
+    const { userId: viewer } = useAuth();
     const navigate = useNavigate();
-
-    const { data, isSuccess } = useQuery({
-        queryKey: ["friends", userId],
-        queryFn: () => axiosInstance.get(`/user/${userId}/friends`),
-    });
-
-    useEffect(() => {
-        if (isSuccess) {
-            setFriends(data.data.friends);
-        }
-    }, [data, isSuccess])
+    const { sentFriendRequests, receivedFriendRequests } = useGetFriendRequests(userId);
+    const { friends } = useGetFriends(userId);
 
     const handleBack = () => {
         navigate(-1);
     };
 
-    const Friend = (friend: UserType) => {
+    const friendRequests = () => {
         return (
-            <div key={friend.id}>
-                <NavLink to={`/user/${friend.id}`}>
-                    {friend.firstName} {friend.lastName}
-                </NavLink>
-            </div>
+            <>
+                <h3>Received friend requests</h3>
+                {receivedFriendRequests.map(friendRequest => <ReceivedFriendRequest key={friendRequest.id} friendRequest={friendRequest}/>)}
+                <h3>Sent friend requests</h3>
+                {sentFriendRequests.map(friendRequest => <SentFriendRequest key={friendRequest.id} friendRequest={friendRequest}/>)}
+            </>
         );
-    }
+    };
 
     return (
         <>
             <h2>Friends</h2>
             <button onClick={handleBack}>Back</button>
-            {friends.map(friend => Friend(friend))}
+            {viewer === userId && friendRequests()}
+            <h3>Friends</h3>
+            {friends.map(friend => <Friend key={friend} friend={friend} />)}
         </>
     );
 };
