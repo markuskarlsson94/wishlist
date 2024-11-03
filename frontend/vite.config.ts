0f541726964209746d7773cbdb451e37df108471
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-03 22:49:21 +0100

    fix(frontend): additional shadcn setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
 		host: true,
 		watch: {
 			usePolling: true,
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
 		},
 	},
 });
