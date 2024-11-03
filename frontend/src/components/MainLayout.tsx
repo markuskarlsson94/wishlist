fedfe806f8989bda5de4b4023d73167df347da7c
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-03 15:19:28 +0100

    frat(frontend): added Bottombar to Mainlayout and layout

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -1,18 +1,22 @@
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Topbar from "./Topbar";
+import Bottombar from "./Bottombar";
 
 const MainLayout = () => {
 	return (
-		<div className="bg-slate-200 flex flex-col min-h-screen">
-			<Topbar />
-			<div className="flex flex-row mx-auto space-x-4">
-				<Sidebar />
-				<div className="w-[600px] bg-white p-3">
-					<Outlet />
+		<>
+			<div className="bg-slate-200 flex flex-col min-h-screen">
+				<Topbar />
+				<div className="flex flex-grow flex-row mx-auto space-x-4 my-4">
+					<Sidebar />
+					<div className="w-[600px] bg-white">
+						<Outlet />
+					</div>
 				</div>
+				<Bottombar />
 			</div>
-		</div>
+		</>
 	);
 };
 
