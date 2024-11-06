9f89fbeb681198d723887ead9c15ef43632ea4ef
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-06 23:34:26 +0100

    feat(frontend): added ItemDialog

---
+++ frontend/src/components/ItemDialog.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import ItemForm from "@/forms/ItemForm";
+import ItemInputType from "@/types/ItemInputType";
+import { useState } from "react";
+
+type ItemDialogConfig = {
+	title: string;
+	submitButtonTitle: string;
+	onSubmit: (input: ItemInputType) => void;
+	values: ItemInputType;
+};
+
+const ItemDialog = ({ config }: { config: ItemDialogConfig }) => {
+	const [open, setOpen] = useState<boolean>(false);
+
+	const onSubmit = (values: ItemInputType) => {
+		config.onSubmit(values);
+		setOpen(false);
+	};
+
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogTrigger asChild>
+				<Button>{config.title}</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>{config.title}</DialogTitle>
+				</DialogHeader>
+				{ItemForm({
+					open,
+					values: config.values,
+					onSubmit,
+					submitButtonTitle: config.submitButtonTitle,
+				})}
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default ItemDialog;
