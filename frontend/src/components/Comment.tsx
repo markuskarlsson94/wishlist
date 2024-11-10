a0bfed677db0f18f39184dc028eac482816bc9b5
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-10 22:23:35 +0100

    feat(frontend): added dropdown menu to comment

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -3,28 +3,36 @@ import { useDeleteComment, useUpdateComment } from "../hooks/comment";
 import CommentType from "../types/CommentType";
 import UpdateCommentForm from "../forms/UpdateCommentForm";
 import CommentInputType from "../types/CommentInputType";
-import { Button } from "./ui/button";
+import { buttonVariants } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import IconButton from "./IconButton";
+import { EllipsisVertical, PencilLine, Trash2 } from "lucide-react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "./ui/alert-dialog";
 
 const Comment = ({ comment, itemId }: { comment: CommentType; itemId: number }) => {
 	const deleteComment = useDeleteComment({ itemId });
 	const updateComment = useUpdateComment({ itemId });
-	const [showUpdate, setShowUpdate] = useState<boolean>(false);
+	const [isEditDialogOpen, setIsEditDialogOpen] = useState<boolean>(false);
+	const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState<boolean>(false);
 
 	const handleDelete = () => {
 		deleteComment(comment.id);
 	};
 
-	const handleShowEdit = () => {
-		setShowUpdate(true);
-	};
-
 	const handleEdit = (newComment: CommentInputType) => {
 		updateComment(comment.id, newComment.comment);
-		setShowUpdate(false);
-	};
-
-	const handleCancel = () => {
-		setShowUpdate(false);
+		setIsEditDialogOpen(false);
 	};
 
 	const isAdmin = comment.isAdmin;
@@ -42,15 +50,69 @@ const Comment = ({ comment, itemId }: { comment: CommentType; itemId: number }) 
 	if (comment.isOwnComment) {
 		return (
 			<>
-				{showUpdate ? (
-					UpdateCommentForm(comment.comment, handleEdit, handleCancel)
-				) : (
-					<>
-						<p>{ownCommentString}</p>
-						<Button onClick={handleShowEdit}>Edit</Button>
-						<Button onClick={handleDelete}>Delete</Button>
-					</>
-				)}
+				<p>{ownCommentString}</p>
+
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<IconButton variant={"ghost"}>
+							<EllipsisVertical />
+						</IconButton>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuItem
+							onClick={() => setIsEditDialogOpen(true)}
+							className="flex justify-between items-center"
+						>
+							<span>Edit</span>
+							<PencilLine />
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() => setIsDeleteDialogOpen(true)}
+							className="flex justify-between items-center"
+						>
+							<span>Delete</span>
+							<Trash2 />
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+
+				<Dialog open={isEditDialogOpen} onOpenChange={() => setIsEditDialogOpen(!isEditDialogOpen)}>
+					<DialogTrigger></DialogTrigger>
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>Edit comment</DialogTitle>
+						</DialogHeader>
+						<UpdateCommentForm
+							config={{
+								comment: comment.comment,
+								onSubmit: handleEdit,
+							}}
+						/>
+					</DialogContent>
+				</Dialog>
+
+				<AlertDialog open={isDeleteDialogOpen} onOpenChange={() => setIsDeleteDialogOpen(!isDeleteDialogOpen)}>
+					<AlertDialogTrigger asChild></AlertDialogTrigger>
+					<AlertDialogContent>
+						<AlertDialogHeader>
+							<AlertDialogTitle>Delete comment</AlertDialogTitle>
+							<AlertDialogDescription>
+								Are you sure you want to permanently delete this comment?
+							</AlertDialogDescription>
+						</AlertDialogHeader>
+						<AlertDialogFooter>
+							<AlertDialogCancel>Cancel</AlertDialogCancel>
+							<AlertDialogAction
+								className={buttonVariants({ variant: "destructive" })}
+								onClick={() => {
+									handleDelete();
+								}}
+							>
+								Delete
+							</AlertDialogAction>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialog>
 			</>
 		);
 	}
