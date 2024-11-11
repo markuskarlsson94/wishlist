7cfc7835e6dbd6626c34320529b1e7ba18e329ed
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-11 22:11:48 +0100

    feat(frontend): added logout button in TopBar

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,5 +1,23 @@
+import { useLogout } from "@/hooks/useLogout";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
+import { LogOut } from "lucide-react";
+
 const Topbar = () => {
-	return <div className="h-[50px] bg-slate-800"></div>;
+	const navigate = useNavigate();
+	const { logout } = useLogout({
+		onSuccess: () => {
+			navigate("/");
+		},
+	});
+
+	return (
+		<div className="h-[50px] bg-slate-800 flex items-center">
+			<Button variant={"secondary"} onClick={() => logout()} className="ml-auto">
+				Logout <LogOut />
+			</Button>
+		</div>
+	);
 };
 
 export default Topbar;
