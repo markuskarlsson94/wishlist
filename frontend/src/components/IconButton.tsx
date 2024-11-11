a4dfaec65e8cd504fa42f89a6163b3a5a4d0fd68
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-11 21:55:12 +0100

    chore(frontend): removed redundant CustomButtonProps

--- frontend/src/components/IconButton.tsx
+++ frontend/src/components/IconButton.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import { Button, type ButtonProps } from "./ui/button";
 import { cn } from "@/lib/utils";
 
-interface CustomButtonProps extends ButtonProps {}
-
-const IconButton = React.forwardRef<HTMLButtonElement, CustomButtonProps>(
-	({ className = "", children, ...props }, ref) => {
-		return (
-			<Button ref={ref} className={cn("relative top-[3px] w-[36px]", className)} {...props}>
-				<>{children}</>
-			</Button>
-		);
-	},
-);
+const IconButton = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className = "", children, ...props }, ref) => {
+	return (
+		<Button ref={ref} className={cn("relative top-[3px] w-[36px]", className)} {...props}>
+			<>{children}</>
+		</Button>
+	);
+});
 
 IconButton.displayName = "IconButton";
 
