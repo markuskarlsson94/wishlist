e3f292206b981690bbcfe110d3cd0e8b1f7b2733
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-12 22:58:52 +0100

    feat(frontend): added EditIcon and DeleteIcon

---
+++ frontend/src/components/icons/DeleteIcon.tsx
@@ -0,0 +1,7 @@
+import { Trash2 } from "lucide-react";
+
+const DeleteIcon = () => {
+	return <Trash2 opacity={0.7} />;
+};
+
+export default DeleteIcon;
---
+++ frontend/src/components/icons/EditIcon.tsx
@@ -0,0 +1,7 @@
+import { PenLine } from "lucide-react";
+
+const EditIcon = () => {
+	return <PenLine opacity={0.7} />;
+};
+
+export default EditIcon;
