fe1d58a44f8001a2690fe9ef3693744288e9e0a2
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-13 21:44:52 +0100

    fix(frontend): added TooltipProvider to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { TooltipProvider } from "./components/ui/tooltip.tsx";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -18,7 +19,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<TooltipProvider>
+				<App />
+			</TooltipProvider>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</React.StrictMode>,
