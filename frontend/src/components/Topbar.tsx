5028f4a9ab9256614d2ae0adcb0ed54047bbc5ff
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-24 11:20:32 +0100

    feat(frontend): made topbar sticky

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -12,7 +12,7 @@ const Topbar = () => {
 	});
 
 	return (
-		<div className="bg-slate-800 flex items-center p-2">
+		<div className="bg-slate-800 flex items-center p-2 sticky z-10 top-0">
 			<Button variant={"secondary"} onClick={() => logout()} className="ml-auto">
 				Logout <LogOut />
 			</Button>
