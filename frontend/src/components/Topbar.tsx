af030f65e125eb9ba59d6d013df088fb5008614a
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-28 23:27:04 +0100

    feat(frontend): added beta badge to Topbar component

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -2,6 +2,8 @@ import { useLogout } from "@/hooks/useLogout";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
 import { LogOut } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { NavLink } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 import LoginDialog from "./LoginDialog";
 import { useEffect } from "react";
@@ -23,6 +25,9 @@ const Topbar = () => {
 
 	return (
 		<div className="bg-slate-800 flex items-center p-2 sticky z-10 top-0">
+			<NavLink to="/beta-info">
+				<Badge className="bg-red-200 text-black hover:bg-red-300">Beta</Badge>
+			</NavLink>
 			{isAuthenticated && (
 				<Button variant={"secondary"} onClick={() => logout()} className="ml-auto">
 					Logout <LogOut />
