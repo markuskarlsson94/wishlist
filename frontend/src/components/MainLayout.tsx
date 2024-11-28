2ef1c23f3812ddd0d2c32cde9ff3e922ea9b2111
Markus Karlsson <markus.karlsson.liu@gmail.com>
2024-11-28 21:59:31 +0100

    feat(frontend): only display sidebar if authenticated

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -2,14 +2,17 @@ import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Topbar from "./Topbar";
 import Bottombar from "./Bottombar";
+import { useAuth } from "@/contexts/AuthContext";
 
 const MainLayout = () => {
+	const { isAuthenticated } = useAuth();
+
 	return (
 		<>
 			<div className="bg-gray-200 flex flex-col min-h-screen">
 				<Topbar />
 				<div className="flex flex-grow flex-row mx-auto space-x-4 my-4">
-					<Sidebar />
+					{isAuthenticated && <Sidebar />}
 					<div className="w-[600px]">
 						<Outlet />
 					</div>
