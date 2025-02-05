cb639e860f394d7c3f9eba6b57e35f4bde134286
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-02-05 21:56:05 +0100

    feat(frontend): added useUpdatePassword hook

--- frontend/src/hooks/password.ts
+++ frontend/src/hooks/password.ts
@@ -3,6 +3,46 @@ import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { useState } from "react";
 
+type UseUpdatePasswordConfig = {
+	onSuccess?: () => void;
+	onError?: (error: AxiosError) => void;
+};
+
+export const useUpdatePassword = (config?: UseUpdatePasswordConfig) => {
+	const mutation = useMutation({
+		mutationFn: ({
+			passwordCur,
+			passwordNew,
+			passwordNewRepeated,
+		}: {
+			passwordCur: string;
+			passwordNew: string;
+			passwordNewRepeated: string;
+		}) =>
+			axiosInstance.post("/user/update-password", {
+				passwordCur,
+				passwordNew,
+				passwordNewRepeated,
+			}),
+		onSuccess: () => {
+			if (config?.onSuccess) {
+				config.onSuccess();
+			}
+		},
+		onError: (error: AxiosError) => {
+			if (config?.onError) {
+				config.onError(error);
+			}
+		},
+	});
+
+	const updatePassword = (passwordCur: string, passwordNew: string, passwordNewRepeated: string) => {
+		mutation.mutate({ passwordCur, passwordNew, passwordNewRepeated });
+	};
+
+	return updatePassword;
+};
+
 type UseRequestPasswordResetConfig = {
 	onSuccess?: (email: string) => void;
 	onError?: (error: AxiosError) => void;
