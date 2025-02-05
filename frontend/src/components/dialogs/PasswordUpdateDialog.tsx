88aa3d8b3ed6bebba097428f08a60cecb48b790c
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-02-05 22:00:06 +0100

    feat(frontend): added PasswordUpdateDialog

---
+++ frontend/src/components/dialogs/PasswordUpdateDialog.tsx
@@ -0,0 +1,36 @@
+import PasswordUpdateForm from "@/forms/PasswordUpdateForm";
+import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { useState } from "react";
+
+type PasswordUpdateDialogConfig = {
+	onSuccess?: () => void;
+};
+
+const PasswordUpdateDialog = (config?: PasswordUpdateDialogConfig) => {
+	const [open, setOpen] = useState<boolean>(false);
+
+	const onSuccess = () => {
+		setOpen(false);
+
+		if (config?.onSuccess) {
+			config.onSuccess();
+		}
+	};
+
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogTrigger asChild>
+				<Button>Update password</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Update password</DialogTitle>
+				</DialogHeader>
+				<PasswordUpdateForm onSuccess={onSuccess} />
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default PasswordUpdateDialog;
