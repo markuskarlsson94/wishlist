7c355f4785507ba50dad0148a3f5d865ac9d41b0
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-02-10 21:36:12 +0100

    feat(frontend): made background white on home page

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -1,15 +1,17 @@
 import Sidebar from "./Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Topbar from "./Topbar";
 import Bottombar from "./Bottombar";
 import { useAuth } from "@/contexts/AuthContext";
 
 const MainLayout = () => {
 	const { isAuthenticated } = useAuth();
+	const location = useLocation();
+	const col = location.pathname === "/" ? "bg_white" : "bg-gray-200";
 
 	return (
 		<>
-			<div className="bg-gray-200 flex flex-col min-h-screen">
+			<div className={`flex flex-col min-h-screen ${col}`}>
 				<Topbar />
 				<div className="flex flex-grow flex-row mx-auto space-x-4 my-4">
 					{isAuthenticated && <Sidebar />}
