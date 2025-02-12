37d098c17a816d9bea6aaa01a3053352d3419c29
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-02-12 12:55:02 +0100

    feat(frontend): moved PasswordUpdatedDialog to PasswordUpdateDialog

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -2,21 +2,14 @@ import { useNavigate } from "react-router-dom";
 import BackButton from "./BackButton";
 import RoundedRect from "./RoundedRect";
 import PasswordUpdateDialog from "./dialogs/PasswordUpdateDialog";
-import PasswordUpdatedDialog from "./dialogs/PasswordUpdatedDialog";
-import { useState } from "react";
 import NameUpdateDialog from "./dialogs/NameUpdateDialog";
 import UserDeleteDialog from "./dialogs/UserDeleteDialog";
 
 const Settings = () => {
 	const navigate = useNavigate();
-	const [passwordUpdatedDialogOpen, setPasswordUpdatedDialogOpen] = useState<boolean>(false);
 
 	const handleBack = () => {
 		navigate(-1);
-	};
-
-	const onPasswordUpdated = () => {
-		setPasswordUpdatedDialogOpen(true);
 	};
 
 	return (
@@ -32,14 +25,12 @@ const Settings = () => {
 				<div className="flex justify-center">
 					<div className="flex flex-col w-80 gap-y-3">
 						<NameUpdateDialog />
-						<PasswordUpdateDialog onSuccess={onPasswordUpdated} />
+						<PasswordUpdateDialog />
 						<div className="h-2" />
 						<UserDeleteDialog />
 					</div>
 				</div>
 			</div>
-
-			<PasswordUpdatedDialog open={passwordUpdatedDialogOpen} setOpen={setPasswordUpdatedDialogOpen} />
 		</RoundedRect>
 	);
 };
--- frontend/src/components/dialogs/PasswordUpdateDialog.tsx
+++ frontend/src/components/dialogs/PasswordUpdateDialog.tsx
@@ -2,6 +2,7 @@ import PasswordUpdateForm from "@/forms/PasswordUpdateForm";
 import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { useState } from "react";
+import PasswordUpdatedDialog from "./PasswordUpdatedDialog";
 
 type PasswordUpdateDialogConfig = {
 	onSuccess?: () => void;
@@ -9,9 +10,11 @@ type PasswordUpdateDialogConfig = {
 
 const PasswordUpdateDialog = (config?: PasswordUpdateDialogConfig) => {
 	const [open, setOpen] = useState<boolean>(false);
+	const [showPasswordUpdatedDialogOpen, setShowPasswordUpdatedDialogOpen] = useState<boolean>(false);
 
 	const onSuccess = () => {
 		setOpen(false);
+		setShowPasswordUpdatedDialogOpen(true);
 
 		if (config?.onSuccess) {
 			config.onSuccess();
@@ -19,17 +22,21 @@ const PasswordUpdateDialog = (config?: PasswordUpdateDialogConfig) => {
 	};
 
 	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogTrigger asChild>
-				<Button>Update Password</Button>
-			</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Update password</DialogTitle>
-				</DialogHeader>
-				<PasswordUpdateForm onSuccess={onSuccess} />
-			</DialogContent>
-		</Dialog>
+		<>
+			<Dialog open={open} onOpenChange={setOpen}>
+				<DialogTrigger asChild>
+					<Button>Update Password</Button>
+				</DialogTrigger>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Update password</DialogTitle>
+					</DialogHeader>
+					<PasswordUpdateForm onSuccess={onSuccess} />
+				</DialogContent>
+			</Dialog>
+
+			<PasswordUpdatedDialog open={showPasswordUpdatedDialogOpen} setOpen={setShowPasswordUpdatedDialogOpen} />
+		</>
 	);
 };
 
