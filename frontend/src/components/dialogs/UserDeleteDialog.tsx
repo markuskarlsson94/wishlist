90237983e1ee0ac22894520e2bc90e8c979290c2
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-02-12 12:45:33 +0100

    feat(frontend): added UserDeleteDialog

---
+++ frontend/src/components/dialogs/UserDeleteDialog.tsx
@@ -0,0 +1,75 @@
+import { AxiosError } from "axios";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { Button, buttonVariants } from "../ui/button";
+import { useAuth } from "@/contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useDeleteUser } from "@/hooks/user";
+import LogoutPromptDialog from "./LogoutPromptDialog";
+import { useState } from "react";
+
+const UserDeleteDialog = () => {
+	const { userId, setIsAuthenticated } = useAuth();
+	const navigate = useNavigate();
+	const [showLogoutPromptDialogOpen, setShowLogoutPromptDialogOpen] = useState<boolean>(false);
+
+	const onDeleteUserSuccess = () => {
+		setIsAuthenticated(false);
+		navigate("/");
+	};
+
+	const onDeleteUserError = (error: AxiosError) => {
+		if (error.response?.status === 403) {
+			setShowLogoutPromptDialogOpen(true);
+		}
+	};
+
+	const deleteUser = useDeleteUser({ onSuccess: onDeleteUserSuccess, onError: onDeleteUserError });
+
+	const onDeleteUser = () => {
+		if (userId !== undefined) {
+			deleteUser(userId);
+		}
+	};
+
+	return (
+		<>
+			<AlertDialog>
+				<AlertDialogTrigger asChild>
+					<Button variant={"destructive"}>Delete Account</Button>
+				</AlertDialogTrigger>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>Delete account</AlertDialogTitle>
+						<AlertDialogDescription>
+							Are you sure you want to permanently delete your account and all associated data? This
+							action can not be undone.
+						</AlertDialogDescription>
+					</AlertDialogHeader>
+					<AlertDialogFooter>
+						<AlertDialogCancel>Cancel</AlertDialogCancel>
+						<AlertDialogAction
+							className={buttonVariants({ variant: "destructive" })}
+							onClick={() => onDeleteUser()}
+						>
+							Delete
+						</AlertDialogAction>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
+
+			<LogoutPromptDialog open={showLogoutPromptDialogOpen} setOpen={setShowLogoutPromptDialogOpen} />
+		</>
+	);
+};
+
+export default UserDeleteDialog;
