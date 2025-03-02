29a949c03d9b816d7d143005541424599ca11d86
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-03-02 22:49:26 +0100

    feat(frontend): updated first news article

--- frontend/src/news.ts
+++ frontend/src/news.ts
@@ -1,3 +1,4 @@
+import { APP_NAME } from "./constants";
 import NewsType from "./types/NewsType";
 
 const news: NewsType[] = [];
@@ -5,9 +6,9 @@ let id = 0;
 
 news.unshift({
 	id: ++id,
-	title: "Hello world",
+	title: `Hello world, ${APP_NAME} is live!`,
 	text: "This is the first news article on this site!",
-	createdAt: new Date("2024-11-30"),
+	createdAt: new Date("2025-03-01"),
 });
 
 export default news;
