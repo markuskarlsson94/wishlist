27f0828b8938dc29a81847d427ded7b726591c24
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-03-02 18:23:29 +0100

    fix(frontend): updated topbar z index

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -30,7 +30,7 @@ const Topbar = () => {
 
 	return (
 		<>
-			<div className="relative bg-slate-800 flex items-center px-3 py-2 sticky top-0">
+			<div className="relative bg-slate-800 flex items-center px-3 py-2 sticky top-0 z-10">
 				<div className="absolute left-3">{isMobile && <AppSidebarTrigger />}</div>
 				{isAuthenticated && (
 					<div className="m-auto">
