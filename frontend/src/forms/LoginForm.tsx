750d9171e3f26a66dc133526074662f6e82a870d
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-05-04 23:24:27 +0200

    chore(frontend): removed unused import

--- frontend/src/forms/LoginForm.tsx
+++ frontend/src/forms/LoginForm.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { AxiosError } from "axios";
 import { StatusCodes } from "http-status-codes";
 import { Info } from "lucide-react";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import z from "zod";
 
