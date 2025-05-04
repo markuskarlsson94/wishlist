3be758fdb5fdae834ebda06998fc625620773b60
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-05-04 22:01:20 +0200

    feat(backend): use status code enum

--- backend/src/errors/errorMessages.js
+++ backend/src/errors/errorMessages.js
@@ -1,351 +1,353 @@
+import { StatusCodes } from "http-status-codes";
+
 const errorMessages = {
 	userAlreadyExists: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "User already exists",
 	},
 
 	wishlistTypeNotFound: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Wishlist type not found",
 	},
 
 	userNotFound: {
-		status: 404,
+		status: StatusCodes.NOT_FOUND,
 		message: "User not found",
 	},
 
 	wishlistNotFound: {
-		status: 404,
+		status: StatusCodes.NOT_FOUND,
 		message: "Wishlist not found",
 	},
 
 	wishlistItemNotFound: {
-		status: 404,
+		status: StatusCodes.NOT_FOUND,
 		message: "Wishlist item not found",
 	},
 
 	reservationNotFound: {
-		status: 404,
+		status: StatusCodes.NOT_FOUND,
 		message: "Reservation not found",
 	},
 
 	refreshTokenRequired: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Refresh token required",
 	},
 
 	commentNotFound: {
-		status: 400,
+		status: StatusCodes.NOT_FOUND,
 		message: "Comment not found",
 	},
 
 	passwordsDontMatch: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "New passwords do not match",
 	},
 
 	oldPasswordIncorrect: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Old password is incorrect",
 	},
 
 	unableToAddItem: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to add item",
 	},
 
 	unableToAddLessThanOneItem: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to add less than one item",
 	},
 
 	amountToReserveTooSmall: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Amount to reserve is too small",
 	},
 
 	amountToReserveTooLarge: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Amount to reserve is too large",
 	},
 
 	unableToReserveOwnItem: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to reserve own item",
 	},
 
 	itemAlreadyReservedByUser: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Item already reserved by user",
 	},
 
 	userAlreadyAddedAsFriend: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "User already added as friend",
 	},
 
 	unableToRemoveFriend: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to remove friend",
 	},
 
 	unableToAddSameUserAsFriend: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to add same user as friend",
 	},
 
 	unableToRemoveSameUserAsFriend: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to remove same user as friend",
 	},
 
 	unableToCreateFriendRequestWithSelf: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to create friend request with self",
 	},
 
 	userQueryTooShort: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "User query too short",
 	},
 
 	missingItemProperties: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Missing item properties",
 	},
 
 	unableToVerifyUser: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Unable to verify User",
 	},
 
 	missingPassword: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Missing password",
 	},
 
 	invalidEmail: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Invalid email",
 	},
 
 	missingUserProperties: {
-		status: 400,
+		status: StatusCodes.BAD_REQUEST,
 		message: "Missing user properties",
 	},
 
 	invalidEmailOrPassword: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Invalid email or password",
 	},
 
 	invalidRefreshToken: {
-		status: 401,
+		status: StatusCodes.UNAUTHORIZED,
 		message: "Invalid refresh token",
 	},
 
 	unauthorizedToDeleteOtherUser: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to delete other user",
 	},
 
 	unauthorizedToAddWishlist: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to add wishlist",
 	},
 
 	unauthorizedToDeleteWishlist: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to delete wishlist",
 	},
 
 	unauthorizedToUpdateWishlist: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to update wishlist",
 	},
 
 	unauthorizedToUpdateWishlistItem: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to update wishlist item",
 	},
 
 	unauthorizedToClearReservations: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to clear reservations",
 	},
 
 	unauthorizedToRemoveReservation: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to remove reservation",
 	},
 
 	unauthorizedToViewReservations: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to view reservations",
 	},
 
 	unauthorizedToAddFriend: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to add friend",
 	},
 
 	unauthorizedToRemoveFriend: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to remove friend",
 	},
 
 	unauthorizedToViewFriends: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to view friends",
 	},
 
 	unauthorizedToCreateFriendRequest: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to create friend request",
 	},
 
 	unauthorizedToRemoveFriendRequest: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to remove friend request",
 	},
 
 	unauthorizedToAcceptFriendRequest: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to accept friend request",
 	},
 
 	unauthorizedToGetFriendRequest: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to get friend request",
 	},
 
 	unauthorizedToGetFriendRequests: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to get friend requests",
 	},
 
 	unauthorizedToAddComment: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to add comment",
 	},
 
 	unauthorizedToRemoveComment: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to remove comment",
 	},
 
 	unauthorizedToUpdateComment: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to update comment",
 	},
 
 	unauthorizedToGetComments: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to get comments",
 	},
 
 	unauthorizedToLogout: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to logout",
 	},
 
 	unauthorizedToUpdatePassword: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to update password",
 	},
 
 	unauthorizedToUpdateUser: {
-		status: 403,
+		status: StatusCodes.FORBIDDEN,
 		message: "Unauthorized to update user",
 	},
 
 	serverError: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Server error",
 	},
 
 	unableToAddNewUser: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to add new user",
 	},
 
 	unableToCreateWishlist: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to create wishlist",
 	},
 
 	unableToGetWishlistsForUser: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to get wishlists for user",
 	},
 
 	unableToCreateFriendRequest: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to create friend request",
 	},
 
 	unableToRemoveFriendRequest: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to remove friend request",
 	},
 
 	unableToAcceptFriendRequest: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to accept friend request",
 	},
 
 	unableToGetFriendRequest: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to get friend request",
 	},
 
 	unableToGetFriendRequests: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to get friend requests",
 	},
 
 	unableToGetUsers: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to get users",
 	},
 
 	unableToAddComment: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to add comment",
 	},
 
 	unableToUpdateComment: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to update comment",
 	},
 
 	unableToRemoveComment: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to remove comment",
 	},
 
 	unableToGetComments: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to get comments",
 	},
 
 	unableToLogout: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to logout",
 	},
 
 	unableToAddPasswordToken: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to add password token",
 	},
 
 	unableToResetPassword: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to reset password",
 	},
 
 	unableToUpdateUser: {
-		status: 500,
+		status: StatusCodes.INTERNAL_SERVER_ERROR,
 		message: "Unable to update user",
 	},
 };
