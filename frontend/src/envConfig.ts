81ced89f54849c26965870c3c69abd914016b8a5
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-05-05 20:06:51 +0200

    feat(frontend): added envConfig

---
+++ frontend/src/envConfig.ts
@@ -0,0 +1,31 @@
+let env: keyof typeof configs = import.meta.env.ENV as keyof typeof configs;
+
+if (!env) {
+	console.warn("Unable to find current environment. Defaulting to 'development'");
+	env = "development";
+}
+
+type ConfigType = {
+	getBackendUrl: () => string;
+};
+
+const devConfig: ConfigType = {
+	getBackendUrl: () => {
+		return `${import.meta.env.VITE_API_URL}:${import.meta.env.VITE_BACKEND_PORT}/api/v1`;
+	},
+};
+
+const prodConfig: ConfigType = {
+	getBackendUrl: () => {
+		return `${import.meta.env.VITE_API_URL}/api/v1`;
+	},
+};
+
+const configs = {
+	development: devConfig,
+	production: prodConfig,
+};
+
+const envConfig: ConfigType = configs[env];
+
+export default envConfig;
