118c52eb437ae3d98cca1a0d35a8246ecb79b96c
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-05-12 21:55:00 +0200

    feat(frontend): added profilePicture utility functions

---
+++ backend/src/utilities/profilePicture.js
@@ -0,0 +1,31 @@
+import ErrorMessage from "../errors/ErrorMessage.js";
+import errorMessages from "../errors/errorMessages.js";
+import logger from "../logger.js";
+import userService from "../services/userService.js";
+import { uploadProfilePicture as upload, removeProfilePicture as remove } from "./profilePictureProvider.js";
+
+export const uploadProfilePicture = async (user, userId, image) => {
+	if (!image) {
+		throw new ErrorMessage(errorMessages.missingImage);
+	}
+
+	const u = await userService.getById(user, userId);
+	const fileName = `${user.id}_${u.firstName}-${u.lastName}_${new Date().toISOString()}`;
+
+	try {
+		return await upload(u.profilePicture, image, fileName);
+	} catch (error) {
+		logger.error(error.message);
+		throw new ErrorMessage(errorMessages.unableToUploadProfilePicture);
+	}
+};
+
+export const removeProfilePicture = async (user, userId) => {
+	const u = await userService.getById(user, userId);
+
+	try {
+		await remove(u.profilePicture);
+	} catch (error) {
+		logger.error(error.message);
+	}
+};
