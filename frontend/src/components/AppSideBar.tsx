b4b74683c8febf7ef18f88d45edbbc0cf3bf8a54
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-05-12 22:28:07 +0200

    feat(frontend): added gradient to app name

--- frontend/src/components/AppSideBar.tsx
+++ frontend/src/components/AppSideBar.tsx
@@ -94,7 +94,7 @@ const AppSidebar = () => {
 					<SidebarGroupContent>
 						<div className="flex justify-center gap-x-3 items-center">
 							<p
-								className="text-xl font-bold text-slate-800 cursor-pointer"
+								className="text-xl font-bold cursor-pointer bg-gradient-to-tr from-blue-600 to-emerald-400 inline-block text-transparent bg-clip-text"
 								onClick={() => navigateTo("/")}
 							>
 								{APP_NAME}
