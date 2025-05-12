419deb8097318d708b96a644cb1f5f465ac583c5
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-05-12 22:26:23 +0200

    fix(frontend): fixed ProfilePictureDialog import

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -4,7 +4,7 @@ import RoundedRect from "./RoundedRect";
 import PasswordUpdateDialog from "./dialogs/PasswordUpdateDialog";
 import NameUpdateDialog from "./dialogs/NameUpdateDialog";
 import UserDeleteDialog from "./dialogs/UserDeleteDialog";
-import ProfilePictureEditor from "./ProfilePictureEditor";
+import ProfilePictureDialog from "./ProfilePictureDialog";
 
 const Settings = () => {
 	const navigate = useNavigate();
@@ -27,7 +27,7 @@ const Settings = () => {
 					<div className="flex flex-col w-80 gap-y-3">
 						<NameUpdateDialog />
 						<PasswordUpdateDialog />
-						<ProfilePictureEditor />
+						<ProfilePictureDialog />
 						<div className="h-2" />
 						<UserDeleteDialog />
 					</div>
