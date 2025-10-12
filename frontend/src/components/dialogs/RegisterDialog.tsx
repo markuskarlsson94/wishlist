388091198d60a76a8656ba4e7feb2e1d389a6864
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-10-12 13:39:00 +0200

    feat(frontend): added GoogleAuthButton to RegisterDialog

--- frontend/src/components/dialogs/RegisterDialog.tsx
+++ frontend/src/components/dialogs/RegisterDialog.tsx
@@ -2,6 +2,8 @@ import RegisterForm from "@/forms/RegisterForm";
 import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { useState } from "react";
+import GoogleAuthButton from "../GoogleAuthButton";
+import SeparatorWithLabel from "../SeparatorWithLabel";
 
 const RegisterDialog = ({
 	setRegistrationConfirmedDialogOpen,
@@ -21,6 +23,11 @@ const RegisterDialog = ({
 				<DialogHeader>
 					<DialogTitle>Register</DialogTitle>
 				</DialogHeader>
+
+				<GoogleAuthButton />
+
+				<SeparatorWithLabel label="Or by email" />
+
 				<RegisterForm
 					setOpen={setOpen}
 					setRegistrationConfirmedDialogOpen={setRegistrationConfirmedDialogOpen}
