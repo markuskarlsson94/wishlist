7f3ba8cba6cf4791b45095ae5576630cd836ce38
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-10-22 23:53:57 +0200

    feat(frontend): connect GoogleAuthButton to Google auth endpoint

--- frontend/src/components/GoogleAuthButton.tsx
+++ frontend/src/components/GoogleAuthButton.tsx
@@ -1,11 +1,15 @@
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "./ui/button";
+import envConfig from "@/envConfig";
 
 const GoogleAuthButton = () => {
 	return (
 		<Button
 			variant="outline"
 			className="flex items-center justify-center w-full py-2 text-sm font-medium border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50"
+			onClick={() => {
+				window.location.href = `${envConfig.getBackendUrl()}/auth/google`;
+			}}
 		>
 			<FcGoogle className="w-5 h-5 mr-2" />
 			Continue with Google
