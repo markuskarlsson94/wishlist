2c24f3c510541189be54b0c8d3960b5dce43db5a
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-10-22 22:45:48 +0200

    feat(backend): added Google passport strategy

--- backend/src/passport.js
+++ backend/src/passport.js
@@ -1,8 +1,14 @@
 import passport from "passport";
 import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import jwt from "jsonwebtoken";
-import { adminRole } from "./roles.js";
+import { adminRole, userRole } from "./roles.js";
 import db from "./db.js";
+import logger from "./logger.js";
+
+export const passportErrors = {
+	userAlreadyExists: "User already exists",
+};
 
 const options = {
 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -23,6 +29,59 @@ passport.use(
 
 		return done(null, false);
 	}),
+);
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: process.env.GOOGLE_CALLBACK_URL,
+		},
+		async (_accessToken, _refreshToken, profile, done) => {
+			try {
+				const googleId = profile.id;
+				const email = profile.emails?.[0]?.value;
+				const firstName = profile.name.givenName;
+				const lastName = profile.name.familyName;
+				const profilePicture = profile.photos?.[0]?.value;
+
+				let user = await db.user.getByGoogleId(googleId);
+
+				if (email) {
+					if (user) {
+						await db.user.updateByGoogleId(googleId, firstName, lastName, profilePicture);
+						logger.info(`User ${user.email} logged in`);
+					} else {
+						const existingUser = await db.user.getByEmail(email);
+
+						if (existingUser) {
+							return done(null, false, {
+								error: passportErrors.userAlreadyExists,
+							});
+						}
+
+						const userId = await db.user.addByGoogleId(
+							profile.id,
+							email,
+							firstName,
+							lastName,
+							profilePicture,
+							userRole(),
+						);
+
+						user = await db.user.getById(userId);
+
+						logger.info(`User ${email} registred by Google`);
+					}
+				}
+
+				done(null, { id: user.id, role: user.role });
+			} catch (err) {
+				done(err, null);
+			}
+		},
+	),
 );
 
 export const isAuthenticated = () => {
