393a73417b70585b8d3bab7556ba23bdcd415db1
Markus Karlsson <markus.karlsson.liu@gmail.com>
2025-11-15 15:32:48 +0100

    chore(backend): added logs for google migration

--- backend/src/migrations/0013_google.js
+++ backend/src/migrations/0013_google.js
@@ -1,10 +1,13 @@
 import { userTable } from "../db.js";
+import logger from "../logger.js";
 
 export async function up(knex) {
 	await knex.schema.table(userTable, (table) => {
 		table.string("googleId");
 		table.setNullable("password");
 	});
+
+	logger.info("Migration 0013_google completed");
 }
 
 export async function down(knex) {
@@ -12,4 +15,6 @@ export async function down(knex) {
 		table.dropColumn("googleId");
 		table.dropNullable("password");
 	});
+
+	logger.info("Migration 0013_google reverted");
 }
