c50280ede2095245773eaf9bde49d051d36d7b75
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-02-05 21:39:06 +0100

    fix(backend): fixed email callback input arguments

--- backend/src/utilities/email.js
+++ backend/src/utilities/email.js
@@ -11,8 +11,8 @@ export const sendVerificationEmail = async (receiver, token) => {
 		[receiver],
 		"Account Verification",
 		`<p>Thank you for signing up at ${process.env.APP_NAME}!</p><p>In order to finish your account setup, you need to follow the link in this email to verify your identity.</p><a href=${link}>Verify your account</a><p>If you did not sign up for ${process.env.APP_NAME}, you can safely ignore this email.</p><p>You can not reply to this message.</p>`,
-		(recipients, data) => {
-			logger.info(`Verfication email sent to ${recipients}. Message id = ${data.MessageId}`);
+		(data) => {
+			logger.info(`Verfication email sent to ${data.recipients}. Message id = ${data.id}`);
 		},
 	);
 };
@@ -26,8 +26,8 @@ export const sendPasswordResetEmail = async (receiver, token) => {
 		[receiver],
 		"Password Reset",
 		`<p>We have received a request to reset your password for your ${process.env.APP_NAME} account. If you made this request, click the link below to set a new password</p><a href=${link}>Reset your password</a><p>This link will expire shortly for safety reasons. If you did not request a password reset, you can safely ignore this email.</p><p>You can not reply to this message.</p>`,
-		(recipients, data) => {
-			logger.info(`Password reset email sent to ${recipients}. Message id = ${data.MessageId}`);
+		(data) => {
+			logger.info(`Password reset email sent to ${data.recipients}. Message id = ${data.id}`);
 		},
 	);
 };
