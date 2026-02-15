7999b5af737e1df58b8c4ad7d57535d25246e20b
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-02-15 20:37:04 +0100

    fix(backend): added default key to resend to be able to run tests

--- backend/src/utilities/emailProvider.js
+++ backend/src/utilities/emailProvider.js
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import logger from "../logger.js";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY ?? "no_key");
 
 export const sendEmail = async (
 	sender,
