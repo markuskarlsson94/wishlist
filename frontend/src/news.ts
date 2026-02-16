c8e8744b9aebd90a2068ba448810fd9464e67677
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-02-16 22:44:36 +0100

    chore(frontend): updated first news article

--- frontend/src/news.ts
+++ frontend/src/news.ts
@@ -7,8 +7,8 @@ let id = 0;
 news.unshift({
 	id: ++id,
 	title: `Hello world, ${APP_NAME} is live!`,
-	text: "This is the first news article on this site!",
-	createdAt: new Date("2025-03-01"),
+	text: `${APP_NAME} has officially been launched onto the world wide web! 🎉`,
+	createdAt: new Date("2026-02-16"),
 });
 
 export default news;
