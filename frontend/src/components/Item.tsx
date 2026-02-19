c9efc580756bf858f019cf7efc69f0b5fba33444
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-02-19 20:03:43 +0100

    feat(frontend): use Navbar in Item

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -26,23 +26,15 @@ import {
 	AlertDialogTitle,
 	AlertDialogTrigger,
 } from "./ui/alert-dialog";
-import BackButton from "./BackButton";
 import EditIcon from "./icons/EditIcon";
 import DeleteIcon from "./icons/DeleteIcon";
 import NotFound from "./NotFound";
-import {
-	Breadcrumb,
-	BreadcrumbEllipsis,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbSeparator,
-} from "./ui/breadcrumb";
 import { useGetWishlist } from "@/hooks/wishlist";
 import { useGetUser } from "@/hooks/user";
 import ProfilePicture from "./ProfilePicture";
 import UserType from "@/types/UserType";
 import Tooltip from "./Tooltip";
+import Navbar from "./Navbar";
 
 const Item = () => {
 	const [isOwner, setIsOwner] = useState<boolean>(false);
@@ -56,6 +48,7 @@ const Item = () => {
 	const deleteReservation = useDeleteReservation({ userId });
 	const { reservation } = useGetReservationByItemId(item?.id);
 	const { user: reserver } = useGetUser(reservation?.[0]?.user);
+	const { user: itemOwner } = useGetUser(item?.owner);
 	const reservedByCurrentUser = reserver?.id === userId;
 	const reservationExists = reservation && reservation?.length > 0;
 	const updateItem = useUpdateItem();
@@ -96,10 +89,6 @@ const Item = () => {
 		if (reservation && item) {
 			deleteReservation(reservation[0].id, item.id);
 		}
-	};
-
-	const handleBack = () => {
-		navigate(-1);
 	};
 
 	const ReserveButton = () => {
@@ -156,150 +145,150 @@ const Item = () => {
 		return <NotFound type="Item" />;
 	}
 
+	const breadcrumbs = () => {
+		return isOwner
+			? [
+					{ title: "My Wishlists", link: `/user/${userId}/wishlists` },
+					{ title: wishlist?.title, link: `/wishlist/${wishlist?.id}` },
+			  ]
+			: [
+					{ title: `${itemOwner?.firstName} ${itemOwner?.lastName}`, link: `/user/${item?.owner}` },
+					{ title: "Wishlists", link: `/user/${item?.owner}/wishlists` },
+					{ title: wishlist?.title, link: `/wishlist/${wishlist?.id}` },
+			  ];
+	};
+
 	return (
-		<RoundedRect>
-			<div className="relative flex items-center justify-between">
-				<BackButton onClick={handleBack} />
-				<div className="absolute left-1/2 transform -translate-x-1/2 font-medium">
-					{wishlist && item && (
-						<Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem>
-									<BreadcrumbEllipsis />
-								</BreadcrumbItem>
-								<BreadcrumbSeparator />
-								<BreadcrumbItem>
-									<BreadcrumbLink asChild>
-										<NavLink to={`/wishlist/${wishlist.id}`}>{wishlist.title}</NavLink>
-									</BreadcrumbLink>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator />
-								<BreadcrumbItem className="text-black text-base">{item.title}</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
+		<div className="flex flex-col gap-y-2">
+			<Navbar breadcrumbs={breadcrumbs()} />
+			<RoundedRect>
+				<div className="flex items-center justify-between">
+					<p className="font-bold">{item?.title}</p>
+					{isOwner && (
+						<div>
+							<DropdownMenu>
+								<DropdownMenuTrigger asChild>
+									<Button size={"icon"} variant={"ghost"}>
+										<EllipsisVertical />
+									</Button>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent align="end">
+									<DropdownMenuItem
+										onClick={() => setIsEditDialogOpen(true)}
+										className="flex justify-between items-center"
+									>
+										<span>Edit</span>
+										<EditIcon />
+									</DropdownMenuItem>
+									<DropdownMenuItem
+										onClick={() => setIsDeleteDialogOpen(true)}
+										className="flex justify-between items-center"
+									>
+										<span>Delete</span>
+										<DeleteIcon />
+									</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenu>
+
+							<Dialog open={isEditDialogOpen} onOpenChange={() => setIsEditDialogOpen(!isEditDialogOpen)}>
+								<DialogTrigger></DialogTrigger>
+								<DialogContent>
+									<DialogHeader>
+										<DialogTitle>Edit item</DialogTitle>
+									</DialogHeader>
+									<ItemForm
+										config={{
+											open: isEditDialogOpen,
+											values: itemValues,
+											onSubmit: onSubmitItem,
+											submitButtonTitle: "Save",
+										}}
+									/>
+								</DialogContent>
+							</Dialog>
+
+							<AlertDialog
+								open={isDeleteDialogOpen}
+								onOpenChange={() => setIsDeleteDialogOpen(!isDeleteDialogOpen)}
+							>
+								<AlertDialogTrigger asChild></AlertDialogTrigger>
+								<AlertDialogContent>
+									<AlertDialogHeader>
+										<AlertDialogTitle>Delete item</AlertDialogTitle>
+										<AlertDialogDescription>
+											Are you sure you want to permanently delete this item?
+										</AlertDialogDescription>
+									</AlertDialogHeader>
+									<AlertDialogFooter>
+										<AlertDialogCancel>Cancel</AlertDialogCancel>
+										<AlertDialogAction
+											className={buttonVariants({ variant: "destructive" })}
+											onClick={() => {
+												handleDelete();
+											}}
+										>
+											Delete
+										</AlertDialogAction>
+									</AlertDialogFooter>
+								</AlertDialogContent>
+							</AlertDialog>
+						</div>
 					)}
 				</div>
-				{isOwner && (
-					<div>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button size={"icon"} variant={"ghost"}>
-									<EllipsisVertical />
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align="end">
-								<DropdownMenuItem
-									onClick={() => setIsEditDialogOpen(true)}
-									className="flex justify-between items-center"
-								>
-									<span>Edit</span>
-									<EditIcon />
-								</DropdownMenuItem>
-								<DropdownMenuItem
-									onClick={() => setIsDeleteDialogOpen(true)}
-									className="flex justify-between items-center"
-								>
-									<span>Delete</span>
-									<DeleteIcon />
-								</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-
-						<Dialog open={isEditDialogOpen} onOpenChange={() => setIsEditDialogOpen(!isEditDialogOpen)}>
-							<DialogTrigger></DialogTrigger>
-							<DialogContent>
-								<DialogHeader>
-									<DialogTitle>Edit item</DialogTitle>
-								</DialogHeader>
-								<ItemForm
-									config={{
-										open: isEditDialogOpen,
-										values: itemValues,
-										onSubmit: onSubmitItem,
-										submitButtonTitle: "Save",
-									}}
-								/>
-							</DialogContent>
-						</Dialog>
-
-						<AlertDialog
-							open={isDeleteDialogOpen}
-							onOpenChange={() => setIsDeleteDialogOpen(!isDeleteDialogOpen)}
+				<div className="my-3"></div>
+				<div className="my-3">
+					<p>{item?.description}</p>
+				</div>
+				{item?.link && (
+					<div className="flex flex-row">
+						<Button
+							variant={"link"}
+							className={"block overflow-hidden whitespace-nowrap text-ellipsis max-w-full"}
 						>
-							<AlertDialogTrigger asChild></AlertDialogTrigger>
-							<AlertDialogContent>
-								<AlertDialogHeader>
-									<AlertDialogTitle>Delete item</AlertDialogTitle>
-									<AlertDialogDescription>
-										Are you sure you want to permanently delete this item?
-									</AlertDialogDescription>
-								</AlertDialogHeader>
-								<AlertDialogFooter>
-									<AlertDialogCancel>Cancel</AlertDialogCancel>
-									<AlertDialogAction
-										className={buttonVariants({ variant: "destructive" })}
-										onClick={() => {
-											handleDelete();
-										}}
-									>
-										Delete
-									</AlertDialogAction>
-								</AlertDialogFooter>
-							</AlertDialogContent>
-						</AlertDialog>
+							<NavLink to={`${item.link}`}>{item.link}</NavLink>
+						</Button>
+						<Tooltip tooltip="Copy link">
+							<Button
+								variant={"ghost"}
+								className="w-9"
+								onClick={() => navigator.clipboard.writeText(item.link || "")}
+							>
+								<Copy />
+							</Button>
+						</Tooltip>
 					</div>
 				)}
-			</div>
-			<div className="my-5">
-				<p>{item?.description}</p>
-			</div>
-			{item?.link && (
-				<div className="flex flex-row">
-					<Button
-						variant={"link"}
-						className={"block overflow-hidden whitespace-nowrap text-ellipsis max-w-full"}
-					>
-						<NavLink to={`${item.link}`}>{item.link}</NavLink>
-					</Button>
-					<Tooltip tooltip="Copy link">
-						<Button
-							variant={"ghost"}
-							className="w-9"
-							onClick={() => navigator.clipboard.writeText(item.link || "")}
-						>
-							<Copy />
-						</Button>
-					</Tooltip>
-				</div>
-			)}
-			<div className="h-6" />
-			<div className="flex flex-col gap-y-3">
-				{item && comments.map((comment) => <Comment key={comment.id} comment={comment} item={item} />)}
-				<div className="flex flex-col gap-y-3 mt-5">
-					{!isOwner && (
-						<p className="flex m-auto text-sm text-gray-400">Your comment will be anonymous to all users</p>
-					)}
-					<AddCommentForm
-						config={{
-							onSubmit: handleAddComment,
-							onCommentChange(comment) {
-								setComment(comment);
-							},
-						}}
-						ref={formRef}
-					/>
-				</div>
-				<div className="flex flex-row">
-					{reserver && !reservedByCurrentUser && <ReservationInfo reserver={reserver} />}
-					<div className="flex gap-x-2 ml-auto">
-						<Button onClick={handleSubmitComment} disabled={comment === ""}>
-							Add comment
-						</Button>
-						{!isOwner && (reservedByCurrentUser ? <UnreserveButton /> : <ReserveButton />)}
+				<div className="h-6" />
+				<div className="flex flex-col gap-y-3">
+					{item && comments.map((comment) => <Comment key={comment.id} comment={comment} item={item} />)}
+					<div className="flex flex-col gap-y-3 mt-5">
+						{!isOwner && (
+							<p className="flex m-auto text-sm text-gray-400">
+								Your comment will be anonymous to all users
+							</p>
+						)}
+						<AddCommentForm
+							config={{
+								onSubmit: handleAddComment,
+								onCommentChange(comment) {
+									setComment(comment);
+								},
+							}}
+							ref={formRef}
+						/>
+					</div>
+					<div className="flex flex-row">
+						{reserver && !reservedByCurrentUser && <ReservationInfo reserver={reserver} />}
+						<div className="flex gap-x-2 ml-auto">
+							<Button onClick={handleSubmitComment} disabled={comment === ""}>
+								Add comment
+							</Button>
+							{!isOwner && (reservedByCurrentUser ? <UnreserveButton /> : <ReserveButton />)}
+						</div>
 					</div>
 				</div>
-			</div>
-		</RoundedRect>
+			</RoundedRect>
+		</div>
 	);
 };
 
