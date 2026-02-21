956881704e3d3d967bb679df14a00de639fb9080
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-02-21 09:47:35 +0100

    feat(frontend): added ability to display profile picture in Navbar breadcrumb

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,10 +10,28 @@ import {
 	BreadcrumbLink,
 } from "./ui/breadcrumb";
 import React from "react";
+import ProfilePicture from "./ProfilePicture";
+import { useGetUser } from "@/hooks/user";
 
 type BreadCrumbType = {
 	title?: string;
 	link?: string;
+	userId?: number;
+};
+
+const BreadcrumbItemCustom = ({ breadcrumb }: { breadcrumb: BreadCrumbType }) => {
+	const { user } = useGetUser(breadcrumb?.userId);
+
+	return breadcrumb.link ? (
+		<BreadcrumbItem>
+			{breadcrumb.userId && <ProfilePicture src={user?.profilePicture} className="h-6 w-6 md:h-8 md:w-8" />}
+			<BreadcrumbLink asChild>
+				<NavLink to={breadcrumb.link}>{breadcrumb.title ? breadcrumb.title : <BreadcrumbEllipsis />}</NavLink>
+			</BreadcrumbLink>
+		</BreadcrumbItem>
+	) : (
+		<BreadcrumbItem>{breadcrumb.title ? breadcrumb.title : <BreadcrumbEllipsis />}</BreadcrumbItem>
+	);
 };
 
 const Navbar = ({ breadcrumbs }: { breadcrumbs?: BreadCrumbType[] }) => {
@@ -27,19 +45,7 @@ const Navbar = ({ breadcrumbs }: { breadcrumbs?: BreadCrumbType[] }) => {
 					<BreadcrumbList className="text-xs md:text-sm">
 						{breadcrumbs?.map((breadcrumb, index) => (
 							<React.Fragment key={breadcrumb.link}>
-								{breadcrumb.link ? (
-									<BreadcrumbItem>
-										<BreadcrumbLink asChild>
-											<NavLink to={breadcrumb.link}>
-												{breadcrumb.title ? breadcrumb.title : <BreadcrumbEllipsis />}
-											</NavLink>
-										</BreadcrumbLink>
-									</BreadcrumbItem>
-								) : (
-									<BreadcrumbItem>
-										{breadcrumb.title ? breadcrumb.title : <BreadcrumbEllipsis />}
-									</BreadcrumbItem>
-								)}
+								<BreadcrumbItemCustom breadcrumb={breadcrumb} />
 								{index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
 							</React.Fragment>
 						))}
