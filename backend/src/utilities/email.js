7361a73792b5831f809eabbd1d5f99063027ea7c
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-02-22 16:41:38 +0100

    chore(backend): reworded password reset email

--- backend/src/utilities/email.js
+++ backend/src/utilities/email.js
@@ -25,7 +25,7 @@ export const sendPasswordResetEmail = async (receiver, token) => {
 		"noreply",
 		[receiver],
 		"Password Reset",
-		`<p>We have received a request to reset your password for your <a href=${url}>${process.env.VITE_APP_NAME}</a> account. If you made this request, click the link below to set a new password</p><a href=${link}>Reset your password</a><p>This link will expire shortly for safety reasons. If you did not request a password reset, you can safely ignore this email.</p><p>You can not reply to this message.</p>`,
+		`<p>We have received a request to reset your password for your <a href=${url}>${process.env.VITE_APP_NAME}</a> account. Follow the link below to set a new password</p><a href=${link}>Reset your password</a><p>This link will expire shortly for safety reasons. You can safely ignore this email if you did not request a password reset.</p><p>You can not reply to this message.</p>`,
 		(data) => {
 			logger.info(`Password reset email sent to ${data.recipients}. Message id = ${data.id}`);
 		},
