1fc7ae9d8d53041367ff890e760466666f798081
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-02-28 21:27:44 +0100

    feat(frontend): adjusted topbar buttons on mobile

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -32,7 +32,7 @@ const Topbar = () => {
 	return (
 		<>
 			<div className="relative bg-slate-800 flex items-center px-3 py-2 sticky top-0 z-10">
-				<div className="absolute left-3">{isMobile && isAuthenticated && <AppSidebarTrigger />}</div>
+				<div className="absolute left-5">{isMobile && isAuthenticated && <AppSidebarTrigger />}</div>
 				{isAuthenticated && (
 					<>
 						<div className="relative m-auto">
@@ -44,7 +44,7 @@ const Topbar = () => {
 							)}
 						</div>
 						{isMobile && (
-							<div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+							<div className="absolute right-5 top-1/2 transform -translate-y-1/2">
 								<Notifications />
 							</div>
 						)}
