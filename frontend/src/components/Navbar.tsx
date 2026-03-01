cb65f8610747f92dd8a3de65b55a46f8d119860c
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-03-01 12:05:07 +0100

    feat(frontend): added isLoading prop to Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,6 +12,7 @@ import React from "react";
 import ProfilePicture from "./ProfilePicture";
 import { useGetUser } from "@/hooks/user";
 import BackButton from "./BackButton";
+import { Skeleton } from "./ui/skeleton";
 
 type BreadCrumbType = {
 	title?: string;
@@ -19,12 +20,17 @@ type BreadCrumbType = {
 	userId?: number;
 };
 
+type BreadCrumbProps = {
+	breadcrumbs: BreadCrumbType[];
+	isLoading?: boolean;
+};
+
 const BreadcrumbItemCustom = ({ breadcrumb }: { breadcrumb: BreadCrumbType }) => {
 	const { user } = useGetUser(breadcrumb?.userId);
 
 	return breadcrumb.link ? (
 		<BreadcrumbItem>
-			{breadcrumb.userId && <ProfilePicture src={user?.profilePicture} className="h-6 w-6 md:h-8 md:w-8" />}
+			{breadcrumb.userId && <ProfilePicture src={user?.profilePicture} className="h-8 w-8" />}
 			<BreadcrumbLink asChild>
 				<NavLink to={breadcrumb.link}>{breadcrumb.title ? breadcrumb.title : <BreadcrumbEllipsis />}</NavLink>
 			</BreadcrumbLink>
@@ -34,27 +40,35 @@ const BreadcrumbItemCustom = ({ breadcrumb }: { breadcrumb: BreadCrumbType }) =>
 	);
 };
 
-const Navbar = ({ breadcrumbs }: { breadcrumbs?: BreadCrumbType[] }) => {
+const Navbar = ({ props }: { props: BreadCrumbProps }) => {
 	const navigate = useNavigate();
 
 	const handleBack = () => {
 		navigate(-1);
 	};
 
+	const breadcrumbs = props.breadcrumbs;
+
 	return (
 		<RoundedRect>
 			<div className="flex gap-x-3">
 				<BackButton onClick={handleBack} />
-				<Breadcrumb className="flex min-h-9 font-medium items-center">
-					<BreadcrumbList className="text-xs md:text-sm">
-						{breadcrumbs?.map((breadcrumb, index) => (
-							<React.Fragment key={breadcrumb.link}>
-								<BreadcrumbItemCustom breadcrumb={breadcrumb} />
-								{index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
-							</React.Fragment>
-						))}
-					</BreadcrumbList>
-				</Breadcrumb>
+				{props?.isLoading ? (
+					<div className="flex items-center">
+						<Skeleton className="h-6 w-[200px]" />
+					</div>
+				) : (
+					<Breadcrumb className="flex min-h-9 font-medium items-center">
+						<BreadcrumbList className="text-xs md:text-sm">
+							{breadcrumbs?.map((breadcrumb, index) => (
+								<React.Fragment key={`${breadcrumb.title}-${index}`}>
+									<BreadcrumbItemCustom breadcrumb={breadcrumb} />
+									{index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+								</React.Fragment>
+							))}
+						</BreadcrumbList>
+					</Breadcrumb>
+				)}
 			</div>
 		</RoundedRect>
 	);
