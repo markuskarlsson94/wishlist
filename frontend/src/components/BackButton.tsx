9cfc25865208cfed703004c98f8ead7493a71b78
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-03-01 18:33:13 +0100

    feat(frontend): added navigation in back button

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { ChevronLeft } from "lucide-react";
 import { Button, type ButtonProps } from "./ui/button";
+import { useNavigate } from "react-router-dom";
 
 const BackButton = React.forwardRef<HTMLButtonElement, ButtonProps>(({ ...props }, ref) => {
+	const navigate = useNavigate();
+
+	const handleBack = () => {
+		navigate(-1);
+	};
+
 	return (
 		<div>
-			<Button size={"icon"} ref={ref} {...props} variant={"secondary"}>
+			<Button size={"icon"} ref={ref} {...props} variant={"secondary"} onClick={handleBack}>
 				<ChevronLeft />
 			</Button>
 		</div>
