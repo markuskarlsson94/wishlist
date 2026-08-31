bf7a2914d290bcfc29b311606cdd094c58035a55
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-08-31 17:02:06 +0200

    feat(frontend): added Infobox

---
+++ frontend/src/components/Infobox.tsx
@@ -0,0 +1,16 @@
+import { Info } from "lucide-react";
+
+const Infobox = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className="flex gap-x-2 bg-slate-100 rounded-lg p-2 mt-2">
+			<div>
+				<Info color="#90a1b9" />
+			</div>
+			<div className="text-sm font-medium m-auto">
+				<p>{children}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Infobox;
