c08e07491091ad5619523c2cd279919d886e1ffa
Markus Karlsson <markus.karlsson.liu@gmail.com>
2026-09-06 21:15:49 +0200

    fix(frontend): added forwardRef to  shadcn InputGroupTextArea

--- frontend/src/components/ui/input-group.tsx
+++ frontend/src/components/ui/input-group.tsx
@@ -142,12 +142,13 @@ function InputGroupInput({
   )
 }
 
-function InputGroupTextarea({
-  className,
-  ...props
-}: React.ComponentProps<"textarea">) {
+const InputGroupTextarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.ComponentProps<"textarea">
+>(({ className, ...props }, ref) => {
   return (
     <Textarea
+      ref={ref}
       data-slot="input-group-control"
       className={cn(
         "flex-1 resize-none rounded-none border-0 bg-transparent py-3 shadow-none focus-visible:ring-0 dark:bg-transparent",
@@ -155,8 +156,8 @@ function InputGroupTextarea({
       )}
       {...props}
     />
-  )
-}
+  );
+});
 
 export {
   InputGroup,
